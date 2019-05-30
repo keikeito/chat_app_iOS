987631369465b85b4641404627e129c20518e9ef
keikeito <n.keikeito0122@gmail.com>
2019-05-30 16:29:48 +0900

    initial commit

---
+++ register_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  register_test
+//
+//  Created by NAKAYAMA KEITO on 2019/05/30.
+//  Copyright © 2019 NAKAYAMA KEITO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ register_testTests/register_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  register_testTests.swift
+//  register_testTests
+//
+//  Created by NAKAYAMA KEITO on 2019/05/30.
+//  Copyright © 2019 NAKAYAMA KEITO. All rights reserved.
+//
+
+import XCTest
+@testable import register_test
+
+class register_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
